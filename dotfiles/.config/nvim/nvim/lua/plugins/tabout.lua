e2b01785d779551d1384d4b405649b80745a1b93
blossilver <chuntiandeng@gmail.com>
2026-06-12 01:04:23 +0800

    feat: add more configs - add aurlist and pkglist and config for nvim

---
+++ dotfiles/.config/nvim/nvim/lua/plugins/tabout.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "abecodes/tabout.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      tabkey = "<Tab>",
+      backwards_tabkey = "<S-Tab>",
+    },
+  },
+}
---
+++ scripts/backup.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+set -e
+
+rsync -a --delete \
+  ~/.config/nvim \
+  ~/myConfig/dotfiles/.config/nvim/
+
+#rsync -a --delete \
+#  ~/.local/share/nvim \
+#  ~/myConfig/dotfiles/.local/share/nvim
+
+#rsync -a --delete \
+#  ~/.local/state/nvim \
+#  ~/myConfig/dotfiles/.local/state/nvim
+
+#rsync -a --delete \
+#  ~/.cache/nvim \
+#  ~/myConfig/dotfiles/.cache/nvim
